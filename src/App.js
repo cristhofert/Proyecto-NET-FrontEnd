eaeb60c23d41ec9fc39b13cb2d6fa8d32adf879d
Cristhofer Travieso <cristhofert97@gmail.com>
2021-12-07 02:08:48 -0300

    tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div classNameName="App">
+      <header classNameName="App-header">
+        <NavBar></NavBar>
+      </header>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button type="button" class="flex items-center px-5 py-2.5 font-medium tracking-wide text-white capitalize   bg-black rounded-md hover:bg-gray-800  focus:outline-none focus:bg-gray-900  transition duration-300 transform active:scale-95 ease-in-out">
+        AAA</button>
         <a
-          className="App-link"
+          classNameName="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
